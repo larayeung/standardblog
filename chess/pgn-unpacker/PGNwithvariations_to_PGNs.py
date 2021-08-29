454eec2c4be96dcdd35c8c361653ffea189cd966
larayeung <69181736+larayeung@users.noreply.github.com>
2021-08-29 09:25:06 -0600

    PGN-unpacker

---
+++ chess/pgn-unpacker/PGNwithvariations_to_PGNs.py
@@ -0,0 +1,10 @@
+
+#open the PGN file 
+
+while True:
+     #print pgn
+     #if not last node on branch:
+         #step forward
+         #continue
+     #else:
+         #either step sideways or step back
