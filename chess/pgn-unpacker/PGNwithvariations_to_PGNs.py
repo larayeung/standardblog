4c89d49e63d6b4c1e4dea106e1e6f4b0753a978d
larayeung <69181736+larayeung@users.noreply.github.com>
2021-08-29 14:41:41 -0600

    hmm

--- chess/pgn-unpacker/PGNwithvariations_to_PGNs.py
+++ chess/pgn-unpacker/PGNwithvariations_to_PGNs.py
@@ -7,53 +7,80 @@ file_to_open = data_folder + "\ModernDefense-Black.pgn"
 reportoirePGN = open(file_to_open)
 
 PGNs_done = []
-PGNs_to_unpack = []
+PGNs_to_walk = []
 
 game = chess.pgn.read_game(reportoirePGN)
 
 #print(game.board().fen())
 #print(game.board())
 
-for item in game.variations:
-    if "(" in str(item):
-        PGNs_to_unpack.append(str(item))
-    else:
-        PGNs_done.append(str(item))
+#return some_PGNs_done, some_PGNs_to_walk
+def separate_into_done_and_towalk(some_game):
+    some_PGNs_done = []
+    some_PGNs_to_walk = []
+    for item in some_game.variations:
+        if "(" in str(item):
+            some_PGNs_to_walk.append(str(item))
+        else:
+            some_PGNs_done.append(str(item))
+    return some_PGNs_done, some_PGNs_to_walk
 
-for pgn in PGNs_to_unpack:
+#return ????
+def walk_each_PGN(some_PGNs_to_walk):
+    some_PGNs_to_game = []
+    for item in some_PGNs_to_walk:
+        pgn2 = io.StringIO(item)
+        game = chess.pgn.read_game(pgn2)
+        game.board().push(game.mainline_moves()[0])
+
+    return some_PGNs_to_game
+
+#initial run, before the loop starts
+ret_PGNs_done, ret_PGNs_to_walk = separate_into_done_and_towalk(game)
+PGNs_done.extend(ret_PGNs_done)
+PGNs_to_walk.extend(ret_PGNs_to_walk)
+#
+
+walked_PGNs = walk_each_PGN(PGNs_to_walk_copy)
+
+PGNs_to_walk_copy = PGNs_to_walk
+PGNs_to_walk = []
+for pgn in PGNs_to_game:
     pgn2 = io.StringIO(pgn)
     game = chess.pgn.read_game(pgn2)
 
     game.board().push(game.mainline_moves()[0])
         
-while True:
-    PGNs_to_unpack_copy = PGNs_to_unpack
-    PGNs_to_unpack = []
-    for pgn in PGNs_to_unpack_copy:
+    for pgn in PGNs_to_walk_copy:
         pgn2 = io.StringIO(pgn)
         game = chess.pgn.read_game(pgn2)
-        for item in game.variations:
-            if "(" in str(item):
-                PGNs_to_unpack.append(str(item))
-                print("found")
-            else:
-                PGNs_done.append(str(item))
-    print("loop")
 
+#in this, you are trying to update PGNs_to_done
+while True:
+
+    #here we are given PGNs_done and PGNs_to_walk
+    ret_PGNs_done, ret_PGNs_to_walk = separate_into_done_and_towalk(game)
+    
+    #they are now added to the main list
+    PGNs_done.extend(ret_PGNs_done)
+    PGNs_to_walk.extend(ret_PGNs_to_walk)
+
+    # if PGNs_to_walk is 0, then this ends
+    '''
+    what does it mean when PGNs_to_walk is 0?
+    its existence suggests that there is a function that updates it
+    '''
+    if len(PGNs_to_walk) == 0:
+        break
+    else:
+        #here, we should empty PGNs_to_walk
+        continue
+    
+    
+
+#we reach this point when we meet these conditions:
+'''
+1. There are no more items in PGNs_to_walk
+'''
 for item in PGNs_done:
     print(item+'\n')
-
-    
-#board = game.board()
-#for move in game.mainline_moves():
-    #board.push(move)
-    #print(board.fen())
-    #print(board)
-    #print(board.fen())
-#while True:
-     #print pgn
-     #if not last node on branch:
-         #step forward
-         #continue
-     #else:
-         #either step sideways or step back
