35f355b5d59da98c98a18702e066f6f5616a1bdc
larayeung <69181736+larayeung@users.noreply.github.com>
2021-08-29 13:12:07 -0600

    forgot that i needed to walk down

--- chess/pgn-unpacker/PGNwithvariations_to_PGNs.py
+++ chess/pgn-unpacker/PGNwithvariations_to_PGNs.py
@@ -1,10 +1,13 @@
 import chess.pgn
 import sys
+import io
 
 data_folder = sys.path[0]
 file_to_open = data_folder + "\ModernDefense-Black.pgn"
-
 reportoirePGN = open(file_to_open)
+
+PGNs_done = []
+PGNs_to_unpack = []
 
 game = chess.pgn.read_game(reportoirePGN)
 
@@ -12,8 +15,35 @@ game = chess.pgn.read_game(reportoirePGN)
 #print(game.board())
 
 for item in game.variations:
-    print(item)
+    if "(" in str(item):
+        PGNs_to_unpack.append(str(item))
+    else:
+        PGNs_done.append(str(item))
 
+for pgn in PGNs_to_unpack:
+    pgn2 = io.StringIO(pgn)
+    game = chess.pgn.read_game(pgn2)
+
+    game.board().push(game.mainline_moves()[0])
+        
+while True:
+    PGNs_to_unpack_copy = PGNs_to_unpack
+    PGNs_to_unpack = []
+    for pgn in PGNs_to_unpack_copy:
+        pgn2 = io.StringIO(pgn)
+        game = chess.pgn.read_game(pgn2)
+        for item in game.variations:
+            if "(" in str(item):
+                PGNs_to_unpack.append(str(item))
+                print("found")
+            else:
+                PGNs_done.append(str(item))
+    print("loop")
+
+for item in PGNs_done:
+    print(item+'\n')
+
+    
 #board = game.board()
 #for move in game.mainline_moves():
     #board.push(move)
