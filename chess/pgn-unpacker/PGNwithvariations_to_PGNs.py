02e0368b091e6a627a8180d651fa28e305f066cb
larayeung <69181736+larayeung@users.noreply.github.com>
2021-08-29 10:17:25 -0600

    recursion gives me anxiety

--- chess/pgn-unpacker/PGNwithvariations_to_PGNs.py
+++ chess/pgn-unpacker/PGNwithvariations_to_PGNs.py
@@ -1,4 +1,4 @@
-import chess
+import chess.pgn
 import sys
 
 data_folder = sys.path[0]
@@ -6,6 +6,20 @@ file_to_open = data_folder + "\ModernDefense-Black.pgn"
 
 reportoirePGN = open(file_to_open)
 
+game = chess.pgn.read_game(reportoirePGN)
+
+#print(game.board().fen())
+#print(game.board())
+
+for item in game.variations:
+    print(item)
+
+#board = game.board()
+#for move in game.mainline_moves():
+    #board.push(move)
+    #print(board.fen())
+    #print(board)
+    #print(board.fen())
 #while True:
      #print pgn
      #if not last node on branch:
